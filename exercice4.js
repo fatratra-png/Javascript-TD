017d597ad176140d27ea81724254812f1af2da2c
fatratra_npc <nyfatratrar@gmail.com>
2026-01-13 11:47:16 +0300

    done 1-12

--- exercice4.js
+++ exercice4.js
@@ -1,5 +1,19 @@
-const mention=(moyenne)=>{
-    const prompt = require("prompt-sync")();
-    const moyenne=Number(prompt('Entrer une note'));
-    switch(moyenne)
-}
+const prompt = require("prompt-sync")();
+const mention = (moyenne) => {
+  switch (true) {
+    case moyenne >= 10 && moyenne < 12:
+      return "Passable";
+    case moyenne >= 12 && moyenne < 14:
+      return "Assez bien";
+    case moyenne >= 14 && moyenne < 16:
+      return "Bien";
+    case moyenne >= 16 && moyenne < 18:
+      return "Tres bien";
+    case moyenne >= 18 && moyenne <= 20:
+      return "Honorable";
+    default:
+      return "Echec";
+  }
+};
+const moyenne = Number(prompt("Entrer votre moyenne: "));
+console.log(mention(moyenne));
